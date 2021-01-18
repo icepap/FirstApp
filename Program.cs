a75fbc291d262fffa7b7d51dcfd86878de2451f8
Solo <email@example.ru>
2021-01-18 15:06:34 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string PersonName;
+            PersonName = "Oraz";
+
+            Console.WriteLine(PersonName);
+            Console.ReadKey();
+        }
+    }
+}
