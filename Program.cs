2afd48d29a1985660f8d6c09478106eb5125c1f2
Solo <email@example.ru>
2021-01-18 16:00:05 +0600

    3.3.1 upd

--- Program.cs
+++ Program.cs
@@ -12,7 +12,20 @@ namespace FirstApp
             Console.WriteLine("Привет, мир!");
             Console.WriteLine("\t Меня зовут \n \t {0}", PersonName);
             Console.WriteLine("\x23 ");
+
+
+            
+            byte age = 22;
+            double shoe_size = 45;
+            bool HaveIApet = false;
+            Console.WriteLine("My name is " + PersonName);
+            Console.WriteLine("My age is " + age);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + shoe_size);
+
             Console.ReadKey();
+
+
         }
     }
 }
